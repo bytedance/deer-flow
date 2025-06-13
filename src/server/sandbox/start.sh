5c8350358ba33cb8d179a82c8be1cdc92e144694
fallengold <zguobd@connect.ust.hk>
2025-06-13 15:50:30 +0800

    Linked to the EAP host in default startup script

--- src/server/sandbox/start.sh
+++ src/server/sandbox/start.sh
@@ -1,1 +1,1 @@
-uv run sandbox.py --host 0.0.0.0 --port 8015 --sandbox_host 127.0.0.1 --sandbox_port 8080
+uv run sandbox.py --host 0.0.0.0 --port 8015 --sandbox_host 10.210.0.52 --sandbox_port 25536 # If connected to EAP
