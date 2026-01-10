e2d09c29ea10a6a4ddac2a62d9bc0e0672878d34
Willem Jiang <willem.jiang@gmail.com>
2026-01-10 21:33:59 +0800

    fix(#355): resolve the issue of sh: dotenv: command not found

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,13 +6,13 @@
 if [ "$1" = "--dev" -o "$1" = "-d" -o "$1" = "dev" -o "$1" = "development" ]; then
   echo -e "Starting DeerFlow in [DEVELOPMENT] mode...\n"
   uv run server.py --reload & SERVER_PID=$$!
-  cd web && pnpm dev & WEB_PID=$$!
+  cd web && pnpm install && pnpm dev & WEB_PID=$$!
   trap "kill $$SERVER_PID $$WEB_PID" SIGINT SIGTERM
   wait
 else
   echo -e "Starting DeerFlow in [PRODUCTION] mode...\n"
   uv run server.py & SERVER_PID=$$!
-  cd web && pnpm start & WEB_PID=$$!
+  cd web && pnpm install && pnpm start & WEB_PID=$$!
   trap "kill $$SERVER_PID $$WEB_PID" SIGINT SIGTERM
   wait
 fi
