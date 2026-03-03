ec4f9d3cac2a348ebb8cc747a4f9ebe9143c99e1
foreleven <for-eleven@hotmail.com>
2026-03-03 14:06:36 +0800

    fix: reorder import of setup_agent for consistency in builtins module

--- backend/src/tools/builtins/__init__.py
+++ backend/src/tools/builtins/__init__.py
@@ -1,6 +1,6 @@
-from .setup_agent_tool import setup_agent
 from .clarification_tool import ask_clarification_tool
 from .present_file_tool import present_file_tool
+from .setup_agent_tool import setup_agent
 from .task_tool import task_tool
 from .view_image_tool import view_image_tool
 
